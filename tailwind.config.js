ab99c3253cf0c45e9ccb20be105f660093daeac9
dimeji003 <oladimejilawal14@gmail.com>
2024-11-04 14:44:18 +0100

    feat: design view winner page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
