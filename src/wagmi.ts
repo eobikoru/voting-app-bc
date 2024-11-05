1f267b115f0da6c5dd9f7a5061a81648bc9e21e6
apple <apple@apples-MacBook-Air.local>
2024-11-05 16:09:30 +0100

    latest

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -6,11 +6,14 @@ import {
   optimism,
   polygon,
   sepolia,
+  baseSepolia,
 } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "RainbowKit App",
   projectId: "YOUR_PROJECT_ID",
-  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia ,baseSepolia,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+  ],
   ssr: true,
 });
