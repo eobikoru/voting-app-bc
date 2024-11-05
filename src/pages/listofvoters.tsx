185b962423803cfb5def8ea9b73e7428947e26ca
dimeji003 <oladimejilawal14@gmail.com>
2024-11-05 23:02:47 +0100

    fix: edit list of proposa and list of votes pages

--- src/pages/listofvoters.tsx
+++ src/pages/listofvoters.tsx
@@ -2,73 +2,60 @@ import React from "react";
 
 const listofvoters = () => {
   return (
-    <div>
-      <div className="max-w-4xl mx-auto px-4">
-        <h1 className="text-3xl text-white my-20 text-center font-bold">
-          List of Voters
-        </h1>
+    <div className='font-raleway'>
+    <div className='bg-black rounded-xl p-10 w-[73vw] m-auto mt-36 '>
+    <h1 className='text-center font-bold text-3xl py-5 text-white'>List Of Voters</h1>
+    <div className="flex flex-col">
+<div className="-m-1.5 overflow-x-auto">
+  <div className="p-1.5 min-w-full inline-block align-middle">
+    <div className="border rounded-lg overflow-hidden dark:border-neutral-700">
+      <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+        <thead className="bg-gray-50 dark:bg-neutral-700">
+          <tr>
+            <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-400">Voters Address</th>
+            <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-400">Name</th>
+            <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-400">Age</th>
+            <th scope="col" className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase dark:text-neutral-400">Action</th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
+          <tr>
+            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">0x4h55j445h6jh6hj6h6j7</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">Dimeji</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">40</td>
+            <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+              <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400">Delete</button>
+            </td>
+          </tr>
 
-        <div className="flex flex-col">
-          <div className="-m-1.5 overflow-x-auto">
-            <div className="p-1.5 min-w-full inline-block align-middle">
-              <div className="overflow-hidden">
-                <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
-                  <thead>
-                    <tr>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-start text-xs font-medium text-white uppercase dark:text-neutral-500"
-                      >
-                        Voters Address
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3  text-start text-xs font-medium text-white uppercase dark:text-neutral-500"
-                      >
-                        Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3  text-start text-xs font-medium text-white uppercase dark:text-neutral-500"
-                      >
-                        Age
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-end text-xs font-medium text-white uppercase dark:text-neutral-500"
-                      >
-                        Action
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="odd:bg-white even:bg-gray-100 hover:bg-gray-100 dark:odd:bg-neutral-800 dark:even:bg-neutral-700 dark:hover:bg-neutral-700">
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
-                        0x67d0e2E540BFdf4c882703006B26E36b88960e5D
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
-                        Tunde
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
-                        24
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-                        <button
-                          type="button"
-                          className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400"
-                        >
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+          <tr>
+            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">Jim Green</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">Donald</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">30</td>
+            <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+              <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400">Delete</button>
+            </td>
+          </tr>
+
+          <tr>
+            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">Joe Black</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">Jane</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">80</td>
+            <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+              <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
+</div>
+</div>
+
+    </div>
+  </div>
+
+
   );
 };
 
