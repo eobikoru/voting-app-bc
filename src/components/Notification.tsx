570e8855a9c4725f95807b41675605bb6b1f2ff9
apple <apple@apples-MacBook-Air.local>
2024-11-07 11:26:01 +0100

    feat

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -6,6 +6,7 @@ const Notification = () => {
       <div className='bg-black rounded-xl h-[30rem] w-[50%]  flex items-center justify-center'>
         <div className='flex flex-col items-center w-[86%]'>
             <div className=" my-7">
+              
             <img src="/image/drm.png "  className="h-[11rem]" alt="sdk"/> 
             </div>
      <p className='text-white  text-[1.6rem] py-3 w-[90%]'>New to MetaMask? Please install the MetaMask extension on your browser and set up a wallet. </p>
