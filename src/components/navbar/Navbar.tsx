3e45b0109f1a2f7b4c086f673c05e847d6b40be8
Olawale Shopeyin <shopeyinwale@gmail.com>
2024-11-07 16:34:54 +0100

    refactoring

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
-import { useAccount } from 'wagmi'
+import { useAccount } from "wagmi";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const account = useAccount()
-  console.log(account.address,"acct");
-  
+  const account = useAccount();
+
   const toggleDropdown = () => {
-    setIsOpen(prev => !prev);
+    setIsOpen((prev) => !prev);
   };
 
   const closeDropdown = () => {
@@ -17,20 +16,20 @@ const Navbar = () => {
 
   // Close dropdown when clicking outside
   useEffect(() => {
-    const handleClickOutside = (event:any) => {
-      if (!event.target.closest('#dropdown')) {
+    const handleClickOutside = (event: any) => {
+      if (!event.target.closest("#dropdown")) {
         closeDropdown();
       }
     };
 
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
   return (
-    <section className='font-raleway'>
+    <section className="font-raleway">
       <nav className="flex justify-between h-[5rem] items-center py-2 px-4 bg-black">
         <Link
           href="/"
@@ -38,51 +37,82 @@ const Navbar = () => {
         >
           Chain Masters
         </Link>
-  {
-   account.address && (
-      <>
-      
-       <div className="relative" id="dropdown">
-          <button 
-            onClick={toggleDropdown} 
-            className="flex  py-2 px-3 items-center gap-x-2 text-sm font-bold text-black-600 border border-gray-400 bg-white rounded-xl p-1 px-2 hover:bg-gray-100 focus:outline-none dark:text-blue-500 dark:hover:bg-gray-700"
-          >
-            Features
-            <svg className="transition-transform duration-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="m6 9 6 6 6-6" />
-            </svg>
-          </button>
+        {account.address && (
+          <>
+            <div className="relative" id="dropdown">
+              <button
+                onClick={toggleDropdown}
+                className="flex  py-2 px-3 items-center gap-x-2 text-sm font-bold text-black-600 border border-gray-400 bg-white rounded-xl p-1 px-2 hover:bg-gray-100 focus:outline-none dark:text-blue-500 dark:hover:bg-gray-700"
+              >
+                Features
+                <svg
+                  className="transition-transform duration-200"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="m6 9 6 6 6-6" />
+                </svg>
+              </button>
 
-          {isOpen && (
-            <div className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-48 bg-white shadow-md rounded-lg">
-              <div className="p-1 space-y-0.5">
-                <Link href="/" className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700" onClick={closeDropdown}>
-                  Home
-                </Link>
-                <Link href="/registervoter" className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700" onClick={closeDropdown}>
-                  Register a voter
-                </Link>
-                <Link href="/createproposal" className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700" onClick={closeDropdown}>
-                  Create Proposal
-                </Link>
-                <Link href="/listofproposal" className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700" onClick={closeDropdown}>
-                 List of Proposal
-                </Link>
+              {isOpen && (
+                <div className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-48 bg-white shadow-md rounded-lg">
+                  <div className="p-1 space-y-0.5">
+                    <Link
+                      href="/"
+                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700"
+                      onClick={closeDropdown}
+                    >
+                      Home
+                    </Link>
+                    <Link
+                      href="/register-voter"
+                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700"
+                      onClick={closeDropdown}
+                    >
+                      Register a voter
+                    </Link>
+                    <Link
+                      href="/create-proposal"
+                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700"
+                      onClick={closeDropdown}
+                    >
+                      Create Proposal
+                    </Link>
+                    <Link
+                      href="/proposals"
+                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700"
+                      onClick={closeDropdown}
+                    >
+                      List of Proposal
+                    </Link>
 
-                <Link href="/listofvoters" className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700" onClick={closeDropdown}>
-                 List of Voters
-                </Link>
-                <Link href="/viewwinner" className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700" onClick={closeDropdown}>
-                  View Winner
-                </Link>
-              </div>
+                    <Link
+                      href="/voters"
+                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700"
+                      onClick={closeDropdown}
+                    >
+                      List of Voters
+                    </Link>
+                    <Link
+                      href="/winner"
+                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700"
+                      onClick={closeDropdown}
+                    >
+                      View Winner
+                    </Link>
+                  </div>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </>
-    )
-  }
-       
+          </>
+        )}
 
         <ConnectButton />
       </nav>
---
+++ src/pages/winner.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { runFireworks } from "../lib/utils";
+import Notification from "../components/Notification";
+import { useAccount } from "wagmi";
+
+const viewwinner = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleList = () => {
+    setIsVisible(!isVisible);
+    runFireworks();
+  };
+  const account = useAccount();
+  console.log(account.address, "acct");
+
+  if (!account.address) {
+    return <Notification />;
+  }
+
+  return (
+    <div className="">
+      <div className="bg-black flex-col flex items-center gap-5 w-[40vw] rounded-xl p-14 m-auto mt-40 font-raleway">
+        <h1 className="text-4xl font-bold text-white">Winning proposal</h1>
+        <button
+          onClick={toggleList}
+          className="bg-purple-800 rounded-sm p-2 w-32 text-sm  text-white text-center"
+        >
+          {isVisible ? "Hide Details" : "View Proposal"}
+        </button>
+        <div
+          className={`flex flex-col gap-6 w-72 items-center bg-black text-white  text-3xl border border-violet-400 rounded-xl p-5 mt-12 ${
+            isVisible ? "flex" : "hidden"
+          } `}
+        >
+          <p>Description: </p>
+          <p>Vote Count: </p>
+          <p>Creator: </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default viewwinner;
