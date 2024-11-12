2ad6e19b4ed640c65720f08f29335f52423dd1e4
apple <apple@apples-MacBook-Air.local>
2024-11-12 13:22:03 +0100

    fixed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,33 +9,33 @@ const Navbar = () => {
   const account = useAccount();
 
   const menu = (
-    <Menu>
-      <Menu.Item key="1" icon={<HomeOutlined />}>
+    <Menu className="p-2">
+      <Menu.Item key="1" icon={<HomeOutlined />} className="px-9 py-4 hover:bg-gray-100 rounded-md">
         <Link href="/" passHref>
           Home
         </Link>
       </Menu.Item>
-      <Menu.Item key="2" icon={<UserAddOutlined />}>
+      <Menu.Item key="4" icon={<UserAddOutlined />} className="px-9 py-4 w-60 h-10 hover:bg-gray-100 rounded-md">
         <Link href="/register-voter" passHref>
           Register a Voter
         </Link>
       </Menu.Item>
-      <Menu.Item key="3" icon={<FileAddOutlined />}>
+      <Menu.Item key="3" icon={<FileAddOutlined />} className="px-9 py-4 hover:bg-gray-100 rounded-md">
         <Link href="/create-proposal" passHref>
           Create Proposal
         </Link>
       </Menu.Item>
-      <Menu.Item key="4" icon={<UnorderedListOutlined />}>
+      <Menu.Item key="4" icon={<UnorderedListOutlined />} className="px-9 w-30 py-4 hover:bg-gray-100 rounded-md">
         <Link href="/proposals" passHref>
           List of Proposals
         </Link>
       </Menu.Item>
-      <Menu.Item key="5" icon={<TeamOutlined />}>
+      <Menu.Item key="5" icon={<TeamOutlined />} className="px-9 py-4 hover:bg-gray-100 rounded-md">
         <Link href="/voters" passHref>
           List of Voters
         </Link>
       </Menu.Item>
-      <Menu.Item key="6" icon={<TrophyOutlined />}>
+      <Menu.Item key="6" icon={<TrophyOutlined />} className="px-9 py-4 hover:bg-gray-100 rounded-md">
         <Link href="/winner" passHref>
           View Winner
         </Link>
@@ -45,16 +45,16 @@ const Navbar = () => {
 
   return (
     <section className="font-raleway">
-      <nav className="fixed top-0 left-0 right-0 z-50 flex justify-between h-[6rem] items-center py-2 px-4 bg-black">
+      <nav className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center h-[6rem] px-6 py-2 bg-gradient-to-r from-gray-900 to-black shadow-lg">
         <Link
           href="/"
-          className="text-3xl font-bold text-white hover:text-gray-800 focus:outline-none ml-5 dark:text-gray-200"
+          className="text-3xl font-bold text-white hover:text-indigo-400 transition-colors duration-300 ml-5 dark:text-gray-200"
         >
           Chain Masters
         </Link>
         {account.address && (
           <Dropdown overlay={menu} trigger={['click']} placement="bottomCenter">
-            <Button className="flex items-center gap-x-2 text-sm font-bold text-black bg-white rounded-xl px-4 py-4 hover:bg-gray-100">
+            <Button className="flex items-center gap-x-2 text-sm font-bold text-black bg-white rounded-xl px-6 py-5 shadow-md hover:bg-gray-200 transition-all duration-300">
               Features <DownOutlined />
             </Button>
           </Dropdown>
--- src/pages/winner.tsx
+++ src/pages/winner.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { runFireworks } from "../lib/utils";
 import Notification from "../components/Notification";
 import { useAccount } from "wagmi";
-import { useReadContract } from "wagmi";
+import { useReadContract, useWriteContract } from "wagmi";
 import { CONTRACT_ABI, CONTRACT_ADDRESS } from "../constant/constant";
+import { message } from "antd";
 
 interface Proposal {
   title: string;
@@ -12,11 +13,50 @@ interface Proposal {
 }
 
 const Winner = () => {
+  const { writeContract, error: errorOne, isError, isPending, isSuccess } = useWriteContract();
+  const { writeContract: contractTwo, error: errorTwo, isError: isErrorTwo, isPending: isPendingTwo, isSuccess: isSuccessTwo } = useWriteContract();
   const [isVisible, setIsVisible] = useState(false);
   const toggleList = () => {
     setIsVisible(!isVisible);
     runFireworks();
   };
+
+  useEffect(() => {
+    if (isSuccess) {
+      message.success({
+        content: "Voting has started.",
+        duration: 3,
+      });
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (errorOne?.message.includes("Only admin can perform this action")) {
+      message.error({
+        content: "Only admin can perform this action.",
+        duration: 4,
+      });
+    }
+  }, [errorOne]);
+
+  useEffect(() => {
+    if (errorTwo?.message.includes("Only admin can perform this action")) {
+      message.error({
+        content: "Only admin can perform this action.",
+        duration: 4,
+      });
+    }
+  }, [errorTwo]);
+
+  useEffect(() => {
+    if (isSuccessTwo) {
+      message.success({
+        content: "Voting has ended.",
+        duration: 3,
+      });
+    }
+  }, [isSuccessTwo]);
+
   const account = useAccount();
   const { data } = useReadContract({
     abi: CONTRACT_ABI,
@@ -24,10 +64,42 @@ const Winner = () => {
     functionName: "getWinner",
   });
 
+  // Fetch isVotingActive data and include the refetch function
+  const { data: isVotingActiveData, refetch } = useReadContract({
+    abi: CONTRACT_ABI,
+    address: CONTRACT_ADDRESS,
+    functionName: "isVotingActive",
+  });
+
+  console.log("isVotingActiveData", isVotingActiveData);
 
   if (!account.address) {
     return <Notification />;
   }
+
+  const handleStartVote = () => {
+    const result = writeContract({
+      abi: CONTRACT_ABI,
+      address: CONTRACT_ADDRESS,
+      functionName: "toggleVoting",
+      args: [true],
+    });
+
+    // Refetch data after starting the vote
+    refetch();
+  };
+
+  const handleEndVote = () => {
+    const result = contractTwo({
+      abi: CONTRACT_ABI,
+      address: CONTRACT_ADDRESS,
+      functionName: "toggleVoting",
+      args: [false],
+    });
+
+    // Refetch data after ending the vote
+    refetch();
+  };
 
 
   const proposalData = data as Proposal | undefined;
@@ -35,32 +107,61 @@ const Winner = () => {
   return (
     <div className="">
       <div className="w-[40vw] mt-40 mx-auto">
+        <div className="bg-white p-6 rounded-lg shadow-md mb-6">
+          <h3 className="text-lg font-semibold mb-4">Voting Controls</h3>
 
-      <div className="bg-white p-6 rounded-lg shadow-md mb-6">
-        <h3 className="text-lg font-semibold mb-4">Voting Controls</h3>
-      
-        <div className="grid grid-cols-2 gap-4">
-          <button
-            className="w-full py-3 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600"
-          >
-            Start Vote
-          </button>
-          <button
-            className="w-full py-3 bg-red-500 text-white rounded-lg shadow-md hover:bg-red-600"
-          >
-            End Vote
-          </button>
+          <div className="grid grid-cols-2 gap-4">
+            <button
+              onClick={() => {
+                if (!(isPending || isVotingActiveData)) {
+                  handleStartVote();
+                }
+              }}
+              className={`w-full py-3 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 ${
+                isPending || isVotingActiveData
+                  ? "opacity-50 cursor-not-allowed"
+                  : "cursor-pointer"
+              }`}
+            >
+              {isPending ? "Starting vote....." : "Start Vote"}
+            </button>
+            <button
+              onClick={() => {
+                if (!(isPendingTwo || !isVotingActiveData)) {
+                  handleEndVote();
+                }
+              }}
+              className={`w-full py-3 bg-red-500 text-white rounded-lg shadow-md hover:bg-red-600 ${
+                isPendingTwo || !isVotingActiveData
+                  ? "opacity-50 cursor-not-allowed"
+                  : "cursor-pointer"
+              }`}
+            >
+              {isPendingTwo ? "Ending vote....." : "End Vote"}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-      <div className="bg-black flex-col flex items-center  mb-20 gap-5 w-[40vw] rounded-xl p-14 m-auto mt-4 font-raleway">
+
+      <div className="bg-black flex-col flex items-center mb-20 gap-5 w-[40vw] rounded-xl p-14 m-auto mt-4 font-raleway">
         <h1 className="text-4xl font-bold text-white">Winning proposal</h1>
-        <button
-          onClick={toggleList}
-          className="bg-purple-800 rounded-sm p-2 w-32 text-sm text-white text-center"
-        >
-          {isVisible ? "Hide Details" : "View Proposal"}
-        </button>
+        {isVotingActiveData === false && proposalData ? (
+          <button
+            onClick={toggleList}
+            className="bg-purple-800 rounded-sm p-2 w-32 text-sm text-white text-center"
+          >
+            {isVisible ? "Hide Details" : "View Proposal"}
+          </button>
+        ) : isVotingActiveData === true ? (
+          <p className="text-white text-lg mt-4">
+            Voting needs to end before the winner can be shown.
+          </p>
+        ) : (
+          <p className="text-white text-lg mt-4">
+            Voting hasn't started or no vote has been made.
+          </p>
+        )}
+
         <div
           className={`flex flex-col gap-6 w-[90%] items-center bg-black text-white text-3xl border border-violet-400 rounded-xl p-5 mt-12 ${
             isVisible ? "flex" : "hidden"
