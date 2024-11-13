bae841a03390a2bbc91e100543a57b7939605011
Olawale Shopeyin <shopeyinwale@gmail.com>
2024-11-13 16:05:44 +0100

    navbar update

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Menu, Dropdown, Button } from "antd";
-import { DownOutlined, HomeOutlined, UserAddOutlined, FileAddOutlined, UnorderedListOutlined, TeamOutlined, TrophyOutlined } from "@ant-design/icons";
+import {
+  DownOutlined,
+  HomeOutlined,
+  UserAddOutlined,
+  FileAddOutlined,
+  UnorderedListOutlined,
+  TeamOutlined,
+  TrophyOutlined,
+} from "@ant-design/icons";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
 import { useAccount } from "wagmi";
@@ -10,32 +18,56 @@ const Navbar = () => {
 
   const menu = (
     <Menu className="p-2">
-      <Menu.Item key="1" icon={<HomeOutlined />} className="px-9 py-4 hover:bg-gray-100 rounded-md">
+      <Menu.Item
+        key="1"
+        icon={<HomeOutlined />}
+        className="px-9 py-4 hover:bg-gray-100 rounded-md"
+      >
         <Link href="/" passHref>
           Home
         </Link>
       </Menu.Item>
-      <Menu.Item key="4" icon={<UserAddOutlined />} className="px-9 py-4 w-60 h-10 hover:bg-gray-100 rounded-md">
+      <Menu.Item
+        key="2"
+        icon={<UserAddOutlined />}
+        className="px-9 py-4 w-60 h-10 hover:bg-gray-100 rounded-md"
+      >
         <Link href="/register-voter" passHref>
           Register a Voter
         </Link>
       </Menu.Item>
-      <Menu.Item key="3" icon={<FileAddOutlined />} className="px-9 py-4 hover:bg-gray-100 rounded-md">
+      <Menu.Item
+        key="3"
+        icon={<FileAddOutlined />}
+        className="px-9 py-4 hover:bg-gray-100 rounded-md"
+      >
         <Link href="/create-proposal" passHref>
           Create Proposal
         </Link>
       </Menu.Item>
-      <Menu.Item key="4" icon={<UnorderedListOutlined />} className="px-9 w-30 py-4 hover:bg-gray-100 rounded-md">
+      <Menu.Item
+        key="4"
+        icon={<UnorderedListOutlined />}
+        className="px-9 w-30 py-4 hover:bg-gray-100 rounded-md"
+      >
         <Link href="/proposals" passHref>
           List of Proposals
         </Link>
       </Menu.Item>
-      <Menu.Item key="5" icon={<TeamOutlined />} className="px-9 py-4 hover:bg-gray-100 rounded-md">
+      <Menu.Item
+        key="5"
+        icon={<TeamOutlined />}
+        className="px-9 py-4 hover:bg-gray-100 rounded-md"
+      >
         <Link href="/voters" passHref>
           List of Voters
         </Link>
       </Menu.Item>
-      <Menu.Item key="6" icon={<TrophyOutlined />} className="px-9 py-4 hover:bg-gray-100 rounded-md">
+      <Menu.Item
+        key="6"
+        icon={<TrophyOutlined />}
+        className="px-9 py-4 hover:bg-gray-100 rounded-md"
+      >
         <Link href="/winner" passHref>
           View Winner
         </Link>
@@ -53,7 +85,7 @@ const Navbar = () => {
           Chain Masters
         </Link>
         {account.address && (
-          <Dropdown overlay={menu} trigger={['click']} placement="bottomCenter">
+          <Dropdown overlay={menu} trigger={["click"]} placement="bottomCenter">
             <Button className="flex items-center gap-x-2 text-sm font-bold text-black bg-white rounded-xl px-6 py-5 shadow-md hover:bg-gray-200 transition-all duration-300">
               Features <DownOutlined />
             </Button>
